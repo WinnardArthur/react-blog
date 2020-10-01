127f4a61065abedb1e54f7a3d7c6dfba90e51ed1
Winnard Arthur <jadenxmith@gmail.com>
2020-10-01 20:37:49 +0000

    moved private infomation into dev.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 5000;
 
+const { User } = require('./models/user')
 
-mongoose.connect("mongodb+srv://JadenXmith:Gibbwizze@react-blog.ls5g2.mongodb.net/<dbname>?retryWrites=true&w=majority", 
+const config = require('./config/keys')
+
+mongoose.connect(config.mongoURI, 
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useCreateIndex: true 
     })
     .then(()=>{
         console.log("Online Database Connected")
@@ -17,8 +23,20 @@ mongoose.connect("mongodb+srv://JadenXmith:Gibbwizze@react-blog.ls5g2.mongodb.ne
     })
 
 
-app.get("/", (req, res)=>{
-    res.send("Hello")
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+
+app.post("/api/users/register", (req, res)=>{
+    const user = new User(req.body)
+
+    user.save((err, userData)=>{
+        if(err)
+            return res.json({ success: false, err})
+    })
+
+    return(res.status(200)).json({success: true})
 })
 
 
